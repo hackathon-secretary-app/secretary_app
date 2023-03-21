8b97ce21c5f368463f9ac786e64d080875460e30
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2023-03-21 21:30:38 +0900

    axiosセットアップ

--- python/routers/map.py
+++ python/routers/map.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 import controllers.map as ctl
-from database import get_db
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, Form, FastAPI, Query, Header
 #routerによるapiの作成
 router = APIRouter()
 
 @router.get("/")
-async def root(db: Session = Depends(get_db)):
+async def root():
  return ctl.get_dbinfo()
